50a2df5e38f965106b63aaa4481f26b6204e4298
Liyunong20000 <li@hnl.t.u-tokyo.ac.jp>
2025-01-20 15:07:47 +0900

    simple change

--- basic_level_sim/src/lite3_basic_sim_pkg/lite3_motion_sim/include/sim_fun.h
+++
@@ -1,7 +0,0 @@
-
-#include "robot_types.h"
-
-#include <vector>
-using namespace std;
-
-void UpdateDate(const vector<double> &joint_angle,const vector<double> & joint_vel,RobotState &RobotState_sim);
